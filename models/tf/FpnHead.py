410a95879d6ee399a047e75be36002466c62d527
robert <winner@pc.com>
2021-03-24 18:42:09 +0200

    Tested tf ConvBn DwConvBn and Upsample

--- models/tf/FpnHead.py
+++ models/tf/FpnHead.py
@@ -1,3 +1,51 @@
 import tensorflow as tf
 from models.tf.common import ConvBn, DwConvBn
 
+
+def UpSample(in_c, mode='interpolate'):
+    if mode == 'conv':
+        # TODO - support tf conv transpose
+        model = None
+    elif mode == 'interpolate':
+        model = tf.keras.Sequential([
+            tf.keras.layers.UpSampling2D(size=2, interpolation='nearest'),
+            ConvBn(filters=in_c, kernel_size=1, strides=1, use_bias=False)
+        ])
+
+    return model
+
+
+class FpnHead(tf.keras.Model):
+    def __init__(self, head_ch, num_laterals, upsample_mode='interpolate', one_feat_map=True):
+        super(FpnHead, self).__init__()
+        
+        self.ups = [
+            UpSample(head_ch, mode=upsample_mode)
+            for _ in range(num_laterals-1)
+        ]
+        self.one_feat_map = one_feat_map
+        self.num_outputs = 1 if one_feat_map else num_laterals
+        self.conv_feats = [
+            ConvBn(filters=head_ch, kernel_size=3, strides=1, use_bias=False)
+            for _ in range(self.num_outputs)
+        ]
+
+    def forward(self, feats):
+        x = feats[0]
+        outs = []
+        for i in range(1, len(feats)):
+            if not self.one_feat_map:
+                outs.append(
+                    self.conv_feats[i-1](x))
+            x = self.ups[i-1](x) + feats[i]
+        outs.append(
+            self.conv_feats[-1](x))
+        return outs
+
+if __name__ == "__main__":
+    input_shape = (2, 32, 32, 24)
+    x = tf.random.normal(input_shape)
+
+    conv = UpSample(24)
+    y = conv(x)
+    print(y.shape)
--- models/tf/common.py
+++ models/tf/common.py
@@ -2,17 +2,31 @@ import tensorflow as tf
 
 
 def ConvBn(*args, **kwargs):
+    pad = kwargs['kernel_size'] // 2
     return tf.keras.Sequential([
+        tf.keras.layers.ZeroPadding2D(pad),
         tf.keras.layers.Conv2D(*args, **kwargs),
-        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.BatchNormalization(epsilon=1e-5),
         tf.keras.layers.ReLU(),
     ])
 
 
 def DwConvBn(*args, **kwargs):
+    pad = kwargs['kernel_size'] // 2
     return tf.keras.Sequential([
+        tf.keras.layers.ZeroPadding2D(pad),
         tf.keras.layers.DepthwiseConv2D(*args, **kwargs),
-        tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.BatchNormalization(epsilon=1e-5),
         tf.keras.layers.ReLU(),
     ])
+
+
+if __name__ == "__main__":
+    input_shape = (2, 32, 32, 3)
+    x = tf.random.normal(input_shape)
+
+    
+    conv = ConvBn(filters=64, kernel_size=3, strides=2)
+    y = conv(x)
+    print(y.shape)
 
